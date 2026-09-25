1f5c4cdbb84544cc8b8ca3d6456842c3f1dd1b33
DaniG <23300039@uttt.edu.mx>
2026-09-25 10:56:38 -0600

    fix: resolucion de error 500 solucionado

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # El error está aquí: 'nombre' no está definido
+    nombre = "Danig" #<- Cambio  
     return f"<h1>Bienvenido al portal universitario, {nombre}!</h1>"
 
 if __name__ == '__main__':
